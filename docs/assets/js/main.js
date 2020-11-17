10604fb62f824f3c7b1676fd55453f5acb57bdfd
saksham1236 <54137431+saksham1236@users.noreply.github.com>
2020-11-17 17:57:09 -0500

    haha

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -21,7 +21,21 @@ jQuery(document).ready(function($){
 				isLateralNavAnimating = false;
 			});
 		}
+  });
+  $('.nav-menu a').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
 	});
+
 
 });
 
